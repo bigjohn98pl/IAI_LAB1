b551b1880f3b9ecd08a04fc5243ef393c5264c72
bigjohn98pl <31948@student.pwsztar.edu.pl>
2019-10-19 20:01:03 +0200

    wstawienie paska nawigacji z html do js jako jeden element

---
+++ script.js
@@ -0,0 +1,3 @@
+let nav = '<ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li><a class="nav-link active" href="#2">Blog</a></li><li><a class="nav-link active" href="#3">Quiz</a></li><li><a class="nav-link active" href="content.html">Kontakt</a></li></ul>';
+let elem = document.querySelector('.navbar');
+elem.innerHTML = nav;
